77e650654f51e39ad63834047621ff36c3c67721
Pawel <pawel.sporna@gmail.com>
2020-01-27 22:49:52 +0100

    Add new exercises

--- resistor-color-duo/resistor_color_duo.py
+++ resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,10 @@
+
 def value(colors):
-    pass
+    coding = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+    result = ''
+    for i in range(2):
+        result += str(coding.index(colors[i]))
+
+    return int(result)
+
+
